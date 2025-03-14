412005da6300d68696c127d3b77330279dc4f73b
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-03-14 09:03:17 +0200

    did some changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 3rd-react-project-weight-convertor
  Me trying to finally apply all that exercise and theory about react
+
+ so right now I added a working converter from kg to lbs
+ what do I wanna do now?
+ I wanna make it so that when the x option in the select element is selected, x conversion happens
--- weight-converter/src/WeightConverter.jsx
+++ weight-converter/src/WeightConverter.jsx
@@ -5,7 +5,7 @@ import React, {useState} from 'react';
 function WeightConverter() {
 
     const [kgs, setKgs] = useState(0);
-
+    const [conversionType, setConversionType] = useState("hello");
     //
     const handleWeightDisplay = (event) => {
             setKgs(event.target.value)
@@ -14,13 +14,32 @@ function WeightConverter() {
     const transformKgToPounds = () => {
             return kgs / 2.2;
     }
-
+    //function to select conversion
+    const handleKgToLbsConversion = () => {
+        if(conversionType === "hello") {
+            alert("hello")
+        }
+    }
+handleKgToLbsConversion()
     return(
-        <div className="converter-container">
-            <h1>Weight Converter</h1>
-            <input type="number" placeholder="Enter weight" onChange={handleWeightDisplay}/>
-            <h2 id="result-display" className='result-display'>{kgs/ 2.2} Pounds</h2>
-        </div>
+        <>
+            <select className='converter-picker'>
+                <option value="KgToPounds">Kg to Pounds</option>
+                <option value="PoundsToKg">Pounds to Kg</option>
+            </select>
+            <div className="converter-container">
+                <h1>Weight Converter</h1>
+                <select className='converter-picker'>
+                <option>Kg to Pounds</option>
+                <option>Pounds to Kg</option>
+            </select>
+            <h2>current conversion is {conversionType}</h2>
+                <input type="number" placeholder="Enter weight" onChange={handleWeightDisplay}/>
+                <h2 id="result-display" className='result-display'>{kgs/ 2.2} Pounds</h2>
+            </div>
+            
+        </>
+        
     )
 }
 
